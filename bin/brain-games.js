b2d1d363710dbfa7790c0140fcead3941c91714b
redchrysler <RedChrysler@protonmail.com>
2020-10-22 21:56:11 +0300

    init project

---
+++ bin/brain-games.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line import/extensions
+import welcome from './src/cli.js';
+
+welcome();
---
+++ bin/src/cli.js
@@ -0,0 +1,8 @@
+import promptly from 'promptly';
+
+const welcome = async () => {
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May I have your name?');
+  console.log(`Hello, ${name}!`);
+};
+export default welcome;
